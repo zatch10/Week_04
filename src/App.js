bae2058c100ff6415d4c53471bbf7eb3479bcec1
arshiam2 <arshiam2@illinois.edu>
2018-09-26 09:25:56 -0500

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return(
+      <div> 
       <h1>Hello World!</h1>
+      <h1>Todo List</h1>
+        <ol>
+          <li> Walk into latea</li>
+          <li> Get boba</li>
+          <li> Get more boba</li>
+        </ol>
+      </div>
     );
   }
 }
